b3e48a9e48b67b3867c6b26a60c3cd170eef90a4
Ismail <34143618+itbeginswithi@users.noreply.github.com>
2022-03-24 08:42:36 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,9 +14,7 @@ class App extends React.Component {
 
   fetchAdvice = async () => {
     try {
-      //   const response = await fetch("https://api.adviceslip.com/advice");
       const response = await axios.get("https://api.adviceslip.com/advice");
-      //   const data = await response.json();
       const {advice} = response.data.slip;
       this.setState({advice});
     } catch (error) {
