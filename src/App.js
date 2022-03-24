b448d3bd0acd3e840a829e0d4a3c8614830c3687
Ismail <ellouismail@gmail.com>
2022-03-24 16:47:00 +0100

    version 1.5

--- src/App.js
+++ src/App.js
@@ -1,39 +1,105 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-import './App.css';
+import Sidebar from "./components/Sidebar";
+import "./App.css";
+import { alreadyAddedToFavs, removeFromFavs } from "./util/handleFavs";
 
-class App extends React.Component {
-  state = {
-    advice: "",
-  };
-  
-    componentDidMount(){
-        this.fetchAdvice()
+const App = () => {
+  const [advice, setAdvice] = useState('');
+  const [adviceArray, setAdviceArray] = useState([]);
+  const [favIcon, setFavIcon] = useState(false);
+  const [increment, setIncrement] = useState(0);
+
+  useEffect(() => {
+    fetchAdvice();
+  }, []);
+
+  useEffect(() => {
+    if (!localStorage.getItem("advices")) {
+      return localStorage.setItem("advices", JSON.stringify([]));
     }
 
-  fetchAdvice = async () => {
+    setAdviceArray(JSON.parse(localStorage.getItem("advices")));
+  }, []);
+
+  const addToFav = () => {
+      const adviceExists = adviceArray.find((saying) => saying === advice);
+
+      if (!adviceExists) {
+        //Adding a new advice to the adviceArray using the set function requires me to add two different advices (a, b) just to add (a) to the localhost for some reason.
+        //That's why I opted for this way instead.
+        adviceArray.push(advice);
+        localStorage.setItem("advices", JSON.stringify(adviceArray));
+
+        /* Using setIncrement to trigger changes in the sidebar component (To show newly added favs) 
+           since Object.is doesnt dig deep into the adviceArray when comparing the old state to the current one.
+           see: https://github.com/facebook/react/issues/15858 
+        */
+        setIncrement(prevState => prevState + 1);
+    }
+  };
+
+  const fetchAdvice = async () => {
     try {
-      //   const response = await fetch("https://api.adviceslip.com/advice");
       const response = await axios.get("https://api.adviceslip.com/advice");
-      //   const data = await response.json();
-      const {advice} = response.data.slip;
-      this.setState({advice});
+      const { advice } = response.data.slip;
+      setAdvice(advice);
+
+      if(alreadyAddedToFavs(advice)){
+        setFavIcon(true);
+      }else{ 
+        setFavIcon(false);
+      }
     } catch (error) {
       console.error(error);
     }
   };
+                                        
+  function clickedBtn(){
+    if(favIcon){ 
+        const {advices, adviceIsRemoved} = removeFromFavs(advice);
+        //remove fav   
+        if(adviceIsRemoved){ 
+            setAdviceArray(advices);
+            setIncrement(prevState => prevState + 1);
+            setFavIcon(false);
+        }
+    }else{                   
+        addToFav();
+        setFavIcon(true);
+    }
+  };
 
-  render() {
-    return (
-        <div className="app">
-            <div className="advice">
-                <h1>{this.state.advice}</h1>
-                <button className="button" onClick={this.fetchAdvice}><span>Refresh</span></button>
-            </div>  
+  return (
+    <div className="app">
+      <div className="advice">
+        <h1>{advice}</h1>
+        <button className="button" onClick={fetchAdvice}>
+          <span>Refresh</span>
+        </button>
+        <div
+          style={{
+            position: "absolute",
+            right: "0",
+            top: "0",
+            cursor: "pointer",
+            borderRadius: "50%",
+            width: '25px',
+            height: '25px',
+            textAlign: "center",
+            lineHeight: "23px",
+            background: favIcon ? "black" : "white",
+            color:  favIcon ? "white" : "black",
+          }}
+          onClick={clickedBtn}
+        >
+          +
         </div>
-    )
-  }
-}
+      </div>
+      <Sidebar newFavAdded={increment} setIncrement={setIncrement} setAdviceArray={setAdviceArray}  advice={advice} setFavIcon={setFavIcon}/>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/util/handleFavs.js
@@ -0,0 +1,23 @@
+const alreadyAddedToFavs = (text) => { 
+    const advices = JSON.parse(localStorage.getItem("advices"));
+    
+    if(advices.find(advice => advice === text)) return true;
+
+    return false;
+}
+
+const removeFromFavs = (text) => { 
+    const advices = JSON.parse(localStorage.getItem("advices"));
+    
+    const adviceIndex = advices.indexOf(text);
+    
+    if(adviceIndex > -1){
+        advices.splice(adviceIndex, 1);
+        localStorage.setItem('advices', JSON.stringify(advices));
+        return {advices, adviceIsRemoved: true, adviceLength: advices.length};
+    }
+    
+    return {adviceIsRemoved: false, adviceLength: advices.length};
+}
+
+export {alreadyAddedToFavs, removeFromFavs};
